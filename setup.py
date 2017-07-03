b446e043738030ce0a2be4c3d7ee09a4ab1c88e9
BuildTools <unconfigured@null.spigotmc.org>
2017-07-03 14:41:59 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 requirements = ["pygeoip"]
 
 setup(
     name='IPTracker',
     version='1.0.0',
-    packages=['iptracker'],
     url='https://github.com/davidomil/IPTracker',
     license='LICENSE.txt',
     author='David Miler',
     author_email='david@orrisystems.com',
     long_description=open('README.md').read(),
-    requires=requirements,
     include_package_data=True,
-    description='Utility to track IP change in realtime'
+    description='Utility to track IP change in realtime',
+    packages=find_packages(exclude=['examples', 'tests']),
+    py_modules=['iptracker'],
+    entry_points={
+        'console_scripts': [
+            'iptracker = iptracker:main',
+        ]
+    },
+    install_requires=requirements
 )
