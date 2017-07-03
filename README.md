17ec274a981c5ffb48b8fef60bf507bd3e86946f
BuildTools <unconfigured@null.spigotmc.org>
2017-07-03 13:49:22 -0400

    Updated readme and created setup.py

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# IPTracker
+# Public IP Tracker
 Small ultility that monitors change in public IP and prints out physical location of the IP address. 
 
-Developed for use with pymultitor and proxychains.
+Developed for use with <a href='https://github.com/realgam3/pymultitor'> pymultitor</a> and proxychains.
---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+requirements = ["pygeoip"]
+
+setup(
+    name='IPTracker',
+    version='1.0.0',
+    packages=['iptracker'],
+    url='https://github.com/davidomil/IPTracker',
+    license='LICENSE.txt',
+    author='David Miler',
+    author_email='david@orrisystems.com',
+    long_description=open('README.md').read(),
+    requires=requirements,
+    include_package_data=True,
+    description='Ultility to track IP change in realtime'
+)
